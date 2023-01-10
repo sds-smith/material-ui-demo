6ac8f674eb82997f1416bf060dde4ddd1db6cc71
Shawn Smith <sds.smith24@gmail.com>
2023-01-10 15:39:18 -0500

    implement and style TourCard

--- src/components/TourCard.jsx
+++ src/components/TourCard.jsx
@@ -1,12 +1,75 @@
 
 import Paper from "@mui/material/Paper"
+import Box from '@mui/material/Box'
 import Grid from "@mui/material/Grid"
+import Typography from "@mui/material/Typography"
+import Rating from "@mui/material/Rating"
+import { AccessTime } from "@mui/icons-material"
+import {createTheme, ThemeProvider} from "@mui/material"
+
+const theme = createTheme({
+    components: {
+        MuiTypography: {
+            variants: [
+                {
+                    props: {
+                        variant: "body2"
+                    },
+                    style: {
+                        fontSize: 11
+                    }
+                },
+                {
+                    props: {
+                        variant: "body3"
+                    },
+                    style: {
+                        fontSize: 9
+                    }
+                },
+            ]
+        }
+    }
+})
 
 
 const TourCard = () => {
     return (
         <Grid item xs={3}>
-            <Paper elevation={12}>Hello</Paper>
+            <ThemeProvider theme={theme}>
+                <Paper elevation={12}>
+                    <img 
+                        src="https://tcproduction.blob.core.windows.net/media/%7B240f8b72-1159-4fd3-a150-0a837f50ba4a%7D.2573758641_297d6d19fa_o.jpg"
+                        alt=""
+                        className='img'
+                    />
+                    <Box padding={1}>
+                        <Typography variant='subtitle1' component='h2'>
+                            Immerse into the Falls
+                        </Typography>
+                        <Box sx={{display: "flex", alignItems: "center"}}>
+                            <AccessTime sx={{width: 12.5}} />
+                            <Typography variant='body2' component='p' marginLeft={0.5}>
+                                5 hours
+                            </Typography>
+                        </Box>
+                        <Box sx={{display: "flex", alignItems: "center"}} marginTop={3}>
+                            <Rating name='read=only' value={4.5} precision={0.5} size='small' readOnly />
+                            <Typography variant='body2' component='p' marginLeft={0.5}>
+                                4.5
+                            </Typography>
+                            <Typography variant='body3' component='p' marginLeft={1.5}>
+                                (655 reviews)
+                            </Typography>
+                        </Box>
+                        <Box>
+                            <Typography variant='h6' component='h3' marginTop={0}>
+                                From $189
+                            </Typography>
+                        </Box>
+                    </Box>
+                </Paper>
+            </ThemeProvider>
         </Grid>
     )
 }
